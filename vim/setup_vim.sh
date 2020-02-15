47d64162ad97d22d0caf10a0c4129e508bdec769
liraop <pedrolira@live.com>
2020-02-14 21:14:30 -0300

    Add vim files

---
+++ vim/setup_vim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+checkVundle() {
+   if [ ! -d ~/.vim/bundle/ ];
+   then
+      printf "Cloning Vundle"
+      git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   fi
+  }
+
+checkVundle
+printf "Vundle is set\nCopying .vimrc\n"
+cp -f .vimrc ~/.vimrc
