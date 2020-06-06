f4029867a1fbf6ae1b2188c8a5246dd81bc68564
liraop <pedrolira@live.com>
2020-06-06 09:59:20 -0300

    Lessons learnt: no symbolic links on git

--- i3/launch_polybar.sh
+++ i3/launch_polybar.sh
@@ -1,1 +1,15 @@
-/home/liraop/.config/i3/launch_polybar.sh
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+CONFIG_PATH=/home/liraop/.config/polybar/example
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+          MONITOR=$m polybar --config=$CONFIG_PATH/config --reload example & 
+  done
+else
+        polybar --config=$CONFIG_PATH/config --reload example & 
+fi
